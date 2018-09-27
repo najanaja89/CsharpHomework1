027aaef869bd9e17e6a4c7a55388ab24c061a4b8
NajaNaja89 <najanaja89@gmail.com>
2018-09-27 17:02:51 +0600

    added comments

--- Program.cs
+++ Program.cs
@@ -9,7 +9,32 @@ namespace CSharp1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+
+            //Вывести имя на консоль
+            Console.WriteLine("Ruslan Tyo");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter number one ");
+            var digitTemp = Console.ReadLine();
+            int numberOne = int.Parse(digitTemp);
+
+            Console.WriteLine("Enter number two ");
+            digitTemp = Console.ReadLine();
+            int numberTwo = int.Parse(digitTemp);
+
+            Console.WriteLine("Enter number Three ");
+            digitTemp = Console.ReadLine();
+            int numberThree = int.Parse(digitTemp);
+
+
+            int sum = numberOne + numberTwo + numberThree;
+
+            Console.WriteLine();
+            Console.Write(sum);
+
+
+            Console.ReadLine();
         }
     }
 }
